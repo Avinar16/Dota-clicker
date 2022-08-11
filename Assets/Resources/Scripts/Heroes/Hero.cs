b51edb37f9dddd766268c4f775e10a70477818cb
blyatifull <99082575+blyatifull@users.noreply.github.com>
2022-08-12 02:12:45 +0300

    Add juggernaut animations + attack sound

--- Assets/Resources/Scripts/Heroes/Hero.cs
+++ Assets/Resources/Scripts/Heroes/Hero.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    [SerializeField]
+    public Animator animator;
     public int Damage;
     protected int exp;
     [SerializeField]
     protected int level = 1;
 
+    bool isAttack = false;
+
+    public AudioClip[] attackSounds;
+    private AudioSource audioSourse;
+
+    private void Start()
+    {
+        audioSourse = GetComponent<AudioSource>();
+    }
+
+    public void attackSoundPlay()
+    {
+        audioSourse.PlayOneShot(attackSounds[Random.Range(0, attackSounds.Length)]);
+        print("’у€ндок");
+    }
 
     void Update()
     {
         Attack();
+        animator.SetBool("isAttack", isAttack);
     }
 
     void Attack()
     {
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                isAttack = true;
+
                 //find creep's script
                 Creep enemy = GameObject.FindGameObjectWithTag("Creep").GetComponent<Creep>();
-
+                
                 enemy.TakeDamage(Damage);
 
                 // Log
                 Debug.Log($"attack {enemy.name} Health= {enemy.health}");
-           }
-    }
 
+            }
+            else
+            {
+                isAttack = false;
+            }
+    }
 }
